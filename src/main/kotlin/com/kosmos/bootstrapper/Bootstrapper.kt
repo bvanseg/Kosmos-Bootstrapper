79fdf1e4d268345f87bbc588230eecf0d4e8f357
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2020-12-21 20:59:30 -0500

    Added logging, program now runs properly.

--- src/main/kotlin/com/kosmos/bootstrapper/Bootstrapper.kt
+++ src/main/kotlin/com/kosmos/bootstrapper/Bootstrapper.kt
@@ -1,11 +1,28 @@
 package com.kosmos.bootstrapper
 
+import bvanseg.kotlincommons.any.getLogger
 import com.kosmos.bootstrapper.plugin.PluginLoader
+import kotlin.system.exitProcess
 
-fun main(vararg args: String) {
-    val location = args[0]
+class Bootstrapper {
 
-    val pluginLoader = PluginLoader(location)
+    companion object {
 
-    pluginLoader.loadAllPlugins()
+        val logger = getLogger()
+
+        @JvmStatic
+        fun main(vararg args: String) {
+
+            if(args.isEmpty()) {
+                logger.warn("No location provided to load plugins from. Aborting...")
+                exitProcess(0)
+            }
+
+            val location = args[0]
+
+            val pluginLoader = PluginLoader(location)
+
+            pluginLoader.loadAllPlugins()
+        }
+    }
 }
