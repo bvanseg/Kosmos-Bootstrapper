e9baba5a196550aa3e9ebcfb7878d14f739bb259
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2020-12-23 14:57:02 -0500

    Engine is now a singleton despite also being a plugin. Hax.

--- game_engine/src/main/kotlin/com/kosmos/engine/KosmosEngine.kt
+++ game_engine/src/main/kotlin/com/kosmos/engine/KosmosEngine.kt
@@ -1,9 +1,15 @@
 package com.kosmos.engine
 
+import bvanseg.kotlincommons.evenir.bus.EventBus
 import com.kosmos.bootstrapper.event.PluginInitializationEvent
 import com.kosmos.bootstrapper.plugin.Plugin
 import com.kosmos.bootstrapper.plugin.PluginLoader
 
+/**
+ * @author Boston Vanseghi
+ * @author Ocelot5836
+ * @since 1.0.0
+ */
 @Plugin(
     version = "1.0.0",
     name = "Kosmos Engine",
@@ -15,7 +21,18 @@ class KosmosEngine {
         PluginLoader.EVENT_BUS.addListener(this)
     }
 
+    companion object {
+        private lateinit var instance: KosmosEngine
+
+        fun getInstance() = instance
+    }
+
+    /**
+     * Primary event bus for the game engine.
+     */
+    val EVENT_BUS = EventBus()
+
     private fun init(event: PluginInitializationEvent) {
-        // TODO:
+        instance = this // Allows plugins dependent on the engine to grab the engine instance.
     }
 }
