d034a7a8877b6f4fed4b479016b98eb0d3c4d9e9
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2020-12-23 14:51:18 -0500

    Added game engine plugin.

---
+++ game_engine/src/main/kotlin/KosmosEngine.kt
@@ -0,0 +1,19 @@
+import com.kosmos.bootstrapper.event.PluginInitializationEvent
+import com.kosmos.bootstrapper.plugin.Plugin
+import com.kosmos.bootstrapper.plugin.PluginLoader
+
+@Plugin(
+    version = "1.0.0",
+    name = "Kosmos Engine",
+    domain = "kosmos_engine"
+)
+class KosmosEngine {
+
+    init {
+        PluginLoader.EVENT_BUS.addListener(this)
+    }
+
+    private fun init(event: PluginInitializationEvent) {
+        // TODO:
+    }
+}
