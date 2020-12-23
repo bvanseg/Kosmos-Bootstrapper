3ed4083d76cd50ad4980b989896a55e50c135400
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2020-12-23 10:52:54 -0500

    Default to working directory if no location arg provided to bootstrapper.

--- bootstrapper/src/main/kotlin/com/kosmos/bootstrapper/Bootstrapper.kt
+++ bootstrapper/src/main/kotlin/com/kosmos/bootstrapper/Bootstrapper.kt
@@ -1,25 +1,14 @@
 package com.kosmos.bootstrapper
 
-import bvanseg.kotlincommons.any.getLogger
 import com.kosmos.bootstrapper.plugin.PluginLoader
-import kotlin.system.exitProcess
 
 internal class Bootstrapper {
 
     companion object {
 
-        private val logger = getLogger()
-
         @JvmStatic
         fun main(vararg args: String) {
-
-            if(args.isEmpty()) {
-                logger.warn("No location provided to load plugins from. Aborting...")
-                exitProcess(0)
-            }
-
-            val location = args[0]
-
+            val location = args.firstOrNull() ?: ""
             PluginLoader.processPluginsAt(location)
         }
     }
